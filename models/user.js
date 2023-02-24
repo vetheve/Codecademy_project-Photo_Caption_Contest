67c4453ca53c834f93dd08b0d6f9b7c6bac2024f
vetheve <victorien.etheve@outlook.fr>
2023-02-24 08:21:19 +0000

    "ADD: Add validation and error handling to User in Sequelize model"

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+// Import the bcrypt library for password hashing
+const { Model } = require('sequelize');
+
+// Import the bcrypt library for password hashing
+const bcrypt = require('bcrypt');
+
+// Define a sequelize model for a User
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -22,25 +27,83 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
     },
     username: {
-      type: DataTypes.STRING(30),
+      type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
-    },
+      unique: {
+        args: true,
+        msg: 'Username already exists'
+      },
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Username is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Username cannot be empty'
+        },
+        len: {
+          args: [3, 30],
+          msg: 'Username must be between 3 and 30 characters'
+        },
+        isAlphanumeric: {
+          args: true,
+          msg: 'Username can only contain letters and numbers'
+        }
+      }
+    },  
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true,
-      },
-    },
-    password:  {
+        isEmail: {
+          args: true,
+          msg: 'Invalid email address'
+        },
+        notNull: {
+          args: true,
+          msg: 'Email is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Email cannot be empty'
+        },
+        max: {
+          args: 50,
+          msg: 'Email is too long'
+        }
+      }
+    },    
+    password: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Password cannot be empty',
+        },
+        len: {
+          args: [8, 20],
+          msg: 'Password must be between 8 and 20 characters long',
+        },
+        not: {
+          args: /(;\")?(--|SELECT|INSERT|UPDATE|DELETE|UNION|EXEC|DROP|ALTER)(\s+)/i,
+          msg: 'Password contains invalid characters',
+        },
+      },
+    },        
   }, {
     sequelize,
     modelName: 'User',
+    // Add hooks to run code before or after certain events
+    hooks: {
+      // Hash the user's password before creating a new user
+      beforeCreate: (User) => {
+        const salt = bcrypt.genSaltSync();
+        User.password = bcrypt.hashSync(User.password, salt);
+      }
+    },
   });
   return User;
 };
