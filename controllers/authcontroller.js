c859b96440711d389cdff50088ce0aa77495d3a4
vetheve <victorien.etheve@outlook.fr>
2023-02-28 11:15:00 +0000

    "FEAT: registerNewUser creates a new user in the database and returns a JWT token"

---
+++ controllers/authcontroller.js
@@ -0,0 +1,84 @@
+// Importing the User model from the index file in the models directory
+const { 
+    User 
+} = require('../models/index.js'); 
+
+// Importing the JSON web token package
+const jwt = require('jsonwebtoken');
+
+// Getting the secret key for the JSON web token from the environment variables
+const secret = process.env.JWT_SECRET;
+
+// Function to register a new user
+exports.registerNewUser = async (req, res) => {
+
+    // Extracting the required fields from the request body
+    const {
+        username,
+        email,
+        password
+    } = req.body;
+
+    try {
+        // Creating a new user in the database
+        const user = await User.create({
+            username,
+            email,
+            password
+        });
+
+        // Creating a JSON web token using the user's ID and the secret key
+        const token = jwt.sign({
+            id: user.id
+        }, secret);
+
+        // Returning the token to the client
+        res.status(201).json({
+            token
+        });
+    } catch (error) {
+        // If an error occurs during the registration process, returning the error message to the client
+        res.status(400).json({
+            error: error.message
+        });
+    }
+};
+
+// Function to log in a user
+exports.loginUser = async (req, res) => {
+
+    // Extracting the email and password from the request body
+    const {
+        email,
+        password
+    } = req.body;
+
+    try {
+        // Finding the user with the given email in the database
+        const user = await User.findOne({
+            where: {
+                email
+            }
+        });
+
+        // If the user is not found in the database or the password is incorrect, throwing an error
+        if (!user || !user.validPassword(password)) {
+            throw new Error('Invalid login');
+        }
+
+        // Creating a JSON web token using the user's ID and the secret key
+        const token = jwt.sign({
+            id: user.id
+        }, secret);
+
+        // Returning the token to the client
+        res.json({
+            token
+        });
+    } catch (error) {
+        // If an error occurs during the login process, returning the error message to the client
+        res.status(401).json({
+            error: error.message
+        });
+    };
+};
---
+++ tests/register/registercontroller.js
@@ -0,0 +1,84 @@
+// Import necessary libraries and dependencies
+const test = require('ava'); // Test runner library
+const sinon = require('sinon'); // Test spies, stubs, and mocks library
+const jwt = require('jsonwebtoken'); // Library to generate and verify JSON Web Tokens
+const { 
+    User 
+} = require('../../models/index.js'); // User model from the application
+
+// Import the function to be tested
+const {
+    registerNewUser
+} = require('../../controllers/authcontroller.js');
+
+// Mock data for the request and response objects
+const req = {
+    body: {
+        username: 'testuser',
+        email: 'testuser@example.com',
+        password: 'password123'
+    }
+};
+
+const res = {
+    status: sinon.stub().returns({
+        json: sinon.spy()
+    })
+};
+
+// Mock secret key for JWT
+const secret = 'testsecret';
+
+// Mock User model's create method
+User.create = sinon.stub().returns({
+    id: 1
+});
+
+// Mock JWT's sign method
+jwt.sign = sinon.stub().returns('testtoken');
+
+// Test Case 1: Test that registerNewUser creates a new user in the database and returns a JWT token
+test('1. registerNewUser creates a new user in the database and returns a JWT token', async (t) => {
+    // Call the function with the mock request and response objects
+    await registerNewUser(req, res);
+
+    // Assert that User.create was called with the correct parameters
+    t.deepEqual(User.create.getCall(0).args[0], {
+        username: 'testuser',
+        email: 'testuser@example.com',
+        password: 'password123'
+    });
+
+    // Print the object in the console
+    t.log(res.body);
+
+    // Assert that JWT.sign was called with the correct parameters
+    t.deepEqual(jwt.sign.getCall(0).args[0], {
+        id: 1
+    });
+    t.is(jwt.sign.getCall(0).args[1], secret);
+
+    // Assert that the response was sent with the correct status code and token
+    t.is(res.status.getCall(0).args[0], 201);
+    t.deepEqual(res.status().json.getCall(0).args[0], {
+        token: 'testtoken'
+    });
+});
+
+// Test Case 2: Test that registerNewUser returns an error message if an error occurs during the registration process
+test('2. registerNewUser returns an error message if an error occurs during the registration process', async (t) => {
+    // Mock User.create to throw an error
+    User.create = sinon.stub().throws(new Error('Database error'));
+
+    // Call the function with the mock request and response objects
+    await registerNewUser(req, res);
+
+    // Print the object in the console
+    t.log(res.status);
+
+    // Assert that the response was sent with the correct status code and error message
+    t.is(res.status.getCall(0).args[0], 400);
+    t.deepEqual(res.status().json.getCall(0).args[0], {
+        error: 'Database error'
+    });
+});
